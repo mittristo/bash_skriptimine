98c17a7f0dc8d767eaf1f3ff331e0d876f093c84
Risto Mitt <mitt.risto@khk.ee>
2020-11-26 14:36:57 +0200

    Kasutaja $ lisamine, Bashile 6ige nimi

--- kasutajad_failist.sh
+++ kasutajad_failist.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-##kasutajate loomine failist
+#   
+# kasutajate lisamise skript
 if [ $# -ne 1 ]; then
     echo "Kasutusjuhend: $0 failinimi"	
 else
-
+failinimi=$1	
   if [ -f $failinimi -a -r $failinimi ]; then
     echo "fail on korras"
     for nimi in $(cat $failinimi)
     do
-      sh lisa_kasutaja $nimi
+      # sellega peaks faili sisu olema nähtav, kui fail on kätte saadav ja nüüd echo asemel tuleb kutsuda vajalik skript
+      # echo $nimi
+      sh lisa_kasutaja.sh $nimi # sellega me laseme lisa_kasutaja skript tööle
     done
   else
     echo "probleem failiga $failinimi"
   fi
-
+# parameetrite kontrolli lõpp
 fi
 
